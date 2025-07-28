6b3d3b5b1432a6d302505f92d66c54468e2bf870
franklin-cruces <franklin.cruces@hotmail.com>
2025-07-28 15:10:29 +0200

    commit inicial

---
+++ otra-reunion-mas/otra-reunion-mas/src/main/java/com/linkedin/learning/otra_reunion_mas/App.java
@@ -0,0 +1,11 @@
+package com.linkedin.learning.otra_reunion_mas;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Version de java: "+System.getProperty("java.version"));
+    }
+}
